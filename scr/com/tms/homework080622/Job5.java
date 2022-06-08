8cb560a09303cf03b91df4accaa2801d5908b7d6
Turtoises <turtoises22@yandex.by>
2022-06-08 12:49:59 +0300

    ctrl+alt+l

--- scr/com/tms/homework080622/Job5.java
+++ scr/com/tms/homework080622/Job5.java
@@ -2,10 +2,10 @@ package com.tms.homework080622;
 
 public class Job5 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int second=345;
-        System.out.println("int minutes= "+(345/60));
-        System.out.println("double minutes="+(345*1.0/60));
+        int second = 345;
+        System.out.println("int minutes= " + (345 / 60));
+        System.out.println("double minutes=" + (345 * 1.0 / 60));
     }
 }
--- scr/com/tms/homework080622/Job6.java
+++ scr/com/tms/homework080622/Job6.java
@@ -1,18 +1,18 @@
 package com.tms.homework080622;
 
 public class Job6 {
-    private static char letterRus='я';
-    private static int indexNextLetter=2;
+    private static char letterRus = 'я';
+    private static int indexNextLetter = 2;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println(getNextLetter(letterRus,indexNextLetter));
+        System.out.println(getNextLetter(letterRus, indexNextLetter));
 
     }
 
-    public static char getNextLetter(char letterRus, int indexNextLetter){
+    public static char getNextLetter(char letterRus, int indexNextLetter) {
 
-        return (char) (letterRus+indexNextLetter);
-     }
+        return (char) (letterRus + indexNextLetter);
+    }
 
 }
